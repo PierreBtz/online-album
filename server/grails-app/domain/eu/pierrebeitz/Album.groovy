5d41e5162f6057c302a3a6a7295921c23b13c98b
PierreBtz <PierreBtz@users.noreply.github.com>
2017-09-17 19:22:26 +0200

    Add a description property for the Album.

--- server/grails-app/domain/eu/pierrebeitz/Album.groovy
+++ server/grails-app/domain/eu/pierrebeitz/Album.groovy
@@ -3,10 +3,12 @@ package eu.pierrebeitz
 class Album {
 
     String name
+    String description
 
     static hasMany = [items: Item]
 
     static constraints = {
         name unique: true
+        description nullable: true
     }
 }
