9e87b7423edf27471183be91e9e26b0b4e1afc84
PierreBtz <PierreBtz@users.noreply.github.com>
2017-09-23 17:09:53 +0200

    Basic add album form

--- client/src/AddAlbum.js
+++ client/src/AddAlbum.js
@@ -1,10 +1,60 @@
 import React, {Component} from 'react';
+import {withStyles} from 'material-ui/styles';
+import PropTypes from 'prop-types';
+import {FormLabel} from 'material-ui/Form';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import 'whatwg-fetch';
+import {SERVER_URL} from './config';
 
 class AddAlbum extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {name: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({name: event.target.value});
+    }
+
+    handleSubmit(event) {
+        const albumName = this.state.name;
+        const data = new FormData();
+        data.append('name', albumName);
+        data.append('description', 'default description');
+        fetch(`${SERVER_URL}/albums`, {
+            method: 'POST',
+            body: data
+        });
+        event.preventDefault();
+    }
+
     render() {
-        <div>hello</div>
+        const classes = this.props.classes;
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <FormLabel>
+                    Name:
+                    <TextField type="text" color="primary" value={this.state.name} onChange={this.handleChange}/>
+                </FormLabel>
+                <br/>
+                <Button type="Submit" raised className={classes.button}>Create</Button>
+            </form>
+        );
     }
 }
 
-export default AddAlbum;
+AddAlbum.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+const styles = theme => ({
+    button: {
+        margin: theme.spacing.unit,
+    }
+});
+
+export default withStyles(styles)(AddAlbum);
