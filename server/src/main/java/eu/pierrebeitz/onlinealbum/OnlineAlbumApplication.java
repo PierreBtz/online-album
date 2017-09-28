6d2f52cd0f5e6ac8e57064e8feeb12780d506584
PierreBtz <PierreBtz@users.noreply.github.com>
2017-09-28 22:23:51 +0200

    Missing files.

---
+++ server/src/main/java/eu/pierrebeitz/onlinealbum/OnlineAlbumApplication.java
@@ -0,0 +1,12 @@
+package eu.pierrebeitz.onlinealbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineAlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineAlbumApplication.class, args);
+	}
+}
