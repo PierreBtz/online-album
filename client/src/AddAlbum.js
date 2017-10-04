8952e43c41d549da19f621735ea1edbc9261e382
PierreBtz <pierrebtz@users.noreply.github.com>
2017-10-04 17:45:25 +0200

    Handle album description in the front end.

--- client/src/AddAlbum.js
+++ client/src/AddAlbum.js
@@ -7,26 +7,32 @@ import Button from 'material-ui/Button';
 import 'whatwg-fetch';
 import {SERVER_URL} from './config';
 
-import { withRouter } from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 
 class AddAlbum extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {name: ''};
+        this.state = {name: '', description: ''};
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({name: event.target.value});
+        const target = event.target;
+        if (target.id === 'name') {
+            this.setState({name: target.value});
+        } else if (target.id === 'description') {
+            this.setState({description: target.value});
+        }
     }
 
     handleSubmit(event) {
         const albumName = this.state.name;
+        const albumDescription = this.state.description;
         const data = new FormData();
         data.append('name', albumName);
-        data.append('description', 'default description');
+        data.append('description', albumDescription);
         fetch(`${SERVER_URL}/albums`, {
             method: 'POST',
             body: data
@@ -37,7 +43,7 @@ class AddAlbum extends Component {
     }
 
     handleResponse(r) {
-        if(r.ok) {
+        if (r.ok) {
             this.props.history.push('/')
         } else {
             console.log('FAILURE');
@@ -50,7 +56,14 @@ class AddAlbum extends Component {
             <form onSubmit={this.handleSubmit}>
                 <FormLabel>
                     Name:
-                    <TextField type="text" color="primary" value={this.state.name} onChange={this.handleChange}/>
+                    <TextField id="name" type="text" color="primary" value={this.state.name}
+                               onChange={this.handleChange}/>
+                </FormLabel>
+                <br/>
+                <FormLabel>
+                    Description:
+                    <TextField id="description" type="text" color="primary" value={this.state.description}
+                               onChange={this.handleChange}/>
                 </FormLabel>
                 <br/>
                 <Button type="Submit" raised className={classes.button}>Create</Button>
